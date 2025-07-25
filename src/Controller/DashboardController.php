149895d58c5805eab4a3ca4fd41d1dccc6992ab7
Sven Heising <sven.heising@gmail.com>
2025-07-25 07:54:09 +0200

    Refactor onboarding detail links to use 'onboardingId' instead of 'id'

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -52,6 +52,8 @@ class DashboardController extends AbstractController
             'active_onboardings' => $this->getActiveOnboardings($entityManager),
             'open_tasks' => $this->getOpenTasks($entityManager),
             'overdue_tasks' => count($this->getOverdueTasks($entityManager)),
+            'total_types' => $this->getTotalOnboardingTypes($entityManager),
+            'total_base_types' => $this->getTotalBaseTypes($entityManager),
         ];
     }
 
@@ -115,4 +117,27 @@ class DashboardController extends AbstractController
             ->getResult();
     }
 
+    /**
+     * Gesamtzahl der OnboardingTypes.
+     */
+    private function getTotalOnboardingTypes(EntityManagerInterface $entityManager): int
+    {
+        return $entityManager->getRepository(OnboardingType::class)
+            ->createQueryBuilder('ot')
+            ->select('COUNT(ot.id)')
+            ->getQuery()
+            ->getSingleScalarResult();
+    }
+
+    /**
+     * Gesamtzahl der BaseTypes.
+     */
+    private function getTotalBaseTypes(EntityManagerInterface $entityManager): int
+    {
+        return $entityManager->getRepository(BaseType::class)
+            ->createQueryBuilder('bt')
+            ->select('COUNT(bt.id)')
+            ->getQuery()
+            ->getSingleScalarResult();
+    }
 }
